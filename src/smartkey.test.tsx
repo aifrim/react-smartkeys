fa4fd3563e798a5a670d6440b2aad0cc1d65d0bf
Alexandru Ifrim <alexandru.ifrim@flowos.com>
2022-11-10 20:28:12 +0200

    feat: react-smartkeys init

---
+++ src/smartkey.test.tsx
@@ -0,0 +1,224 @@
+import '@testing-library/jest-dom'
+import { act, fireEvent, render } from '@testing-library/react'
+import { parseHotkey } from 'is-hotkey'
+import React, { useMemo, useState } from 'react'
+import Smartkeys, { useGroupedSmartkeys, useSmartkeys } from '.'
+
+jest.setTimeout(3000)
+
+const sleep = async (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+describe('Smartkeys', () => {
+  it('should be truthy', () => {
+    expect(Smartkeys.Provider).toBeTruthy()
+  })
+
+  it('should render', () => {
+    const testMessage = 'Hello world'
+
+    const Test = () => {
+      return <Smartkeys.Provider>{testMessage}</Smartkeys.Provider>
+    }
+
+    const container = render(<Test />)
+
+    expect(container.getByText(testMessage)).toBeInTheDocument()
+  })
+
+  it('should register/unregister hotkey', () => {
+    const keyDef = {
+      key: 'mod+k',
+      group: 'main'
+    }
+
+    const TestSmartkeyConsumer = () => {
+      const calledKey = useSmartkeys(keyDef)
+
+      return <div>{calledKey}</div>
+    }
+
+    const Test = () => {
+      const [registered, setRegister] = useState(true)
+
+      return (
+        <Smartkeys.Provider>
+          {registered ? <TestSmartkeyConsumer /> : <div>no-key</div>}
+          <button onClick={() => setRegister(false)}>Unregister</button>
+        </Smartkeys.Provider>
+      )
+    }
+
+    const container = render(<Test />)
+
+    fireEvent.keyUp(container.baseElement, {
+      ctrlKey: true,
+      key: 'k'
+    })
+
+    expect(container.getByText(keyDef.key)).toBeInTheDocument()
+
+    fireEvent.click(container.getByText('Unregister'), {})
+
+    expect(container.getByText('no-key')).toBeInTheDocument()
+  })
+
+  it('should register multiple hotkeys', async () => {
+    const keyList = [
+      {
+        key: 'mod+k',
+        group: 'main'
+      },
+      {
+        key: 'shift+a',
+        group: 'main'
+      }
+    ]
+
+    const TestSmartkeyConsumer = () => {
+      const calledKey = useSmartkeys(keyList)
+
+      return <div>{calledKey}</div>
+    }
+
+    const Test = () => {
+      return (
+        <Smartkeys.Provider>
+          <TestSmartkeyConsumer />
+        </Smartkeys.Provider>
+      )
+    }
+
+    const container = render(<Test />)
+
+    fireEvent.keyUp(container.baseElement, {
+      ctrlKey: true,
+      key: 'k'
+    })
+
+    expect(container.getByText(keyList[0].key)).toBeInTheDocument()
+
+    await act(async () => {
+      await sleep(1000)
+    })
+
+    act(() => {
+      fireEvent.keyUp(container.baseElement, {
+        shiftKey: true,
+        key: 'a'
+      })
+    })
+
+    expect(container.getByText(keyList[1].key)).toBeInTheDocument()
+  })
+
+  it('should chain multiple hotkeys', async () => {
+    const keyDef = {
+      key: 'mod+k alt+a',
+      group: 'main'
+    }
+
+    const TestSmartkeyConsumer = () => {
+      const calledKey = useSmartkeys(keyDef)
+
+      return <div>{calledKey}</div>
+    }
+
+    const Test = () => {
+      return (
+        <Smartkeys.Provider>
+          <TestSmartkeyConsumer />
+        </Smartkeys.Provider>
+      )
+    }
+
+    const container = render(<Test />)
+
+    fireEvent.keyUp(container.baseElement, {
+      ctrlKey: true,
+      key: 'k'
+    })
+
+    await act(async () => {
+      await sleep(100)
+    })
+
+    fireEvent.keyUp(container.baseElement, {
+      altKey: true,
+      key: 'a'
+    })
+
+    expect(container.getByText(keyDef.key)).toBeInTheDocument()
+  })
+
+  it('should register all available hotkeys', async () => {
+    const keyDef1 = {
+      key: 'ctrl+b'
+    }
+
+    const keyDef2 = {
+      key: 'mod+k alt+a',
+      passthrough: true
+    }
+
+    const getStr = (obj) =>
+      Object.entries(obj)
+        .map(([k, v]) => [k, typeof v === 'object' ? getStr(v) : v])
+        .flat()
+        .join(':')
+
+    const TestSmartkeyConsumer1 = () => {
+      const keyDef = useMemo(() => ({ ...keyDef1, group: 'main' }), [])
+
+      useSmartkeys(keyDef)
+
+      return null
+    }
+
+    const TestSmartkeyConsumer2 = () => {
+      const keyDef = useMemo(() => ({ ...keyDef2, group: 'main' }), [])
+
+      useSmartkeys(keyDef)
+
+      return null
+    }
+
+    const TestSmartkeyGroupConsumer = () => {
+      const groups = useGroupedSmartkeys()
+
+      return (
+        <div>
+          {Object.entries(groups).map(([group, defs]) =>
+            defs.map((def) => (
+              <div key={`${group}:${def.key}`}>{getStr(def)}</div>
+            ))
+          )}
+        </div>
+      )
+    }
+
+    const Test = () => {
+      return (
+        <Smartkeys.Provider>
+          <TestSmartkeyConsumer1 />
+          <TestSmartkeyConsumer2 />
+
+          <TestSmartkeyGroupConsumer />
+        </Smartkeys.Provider>
+      )
+    }
+
+    const container = render(<Test />)
+
+    const getDef = (def) => ({
+      ...def,
+      hotkey:
+        def.key.indexOf(' ') > 0
+          ? def.key.split(' ').map((k) => parseHotkey(k))
+          : parseHotkey(def.key)
+    })
+
+    expect(container.getByText(getStr(getDef(keyDef1)))).toBeInTheDocument()
+    expect(container.getByText(getStr(getDef(keyDef2)))).toBeInTheDocument()
+  })
+})
---
+++ src/smartkeys.tsx
@@ -0,0 +1,198 @@
+import { HotKey, parseHotkey } from 'is-hotkey'
+import React, {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react'
+
+type Callback = () => void
+
+type Groups = string
+
+type SmartkeyDefinition = {
+  key: string
+  passthrough?: boolean
+  group: Groups
+}
+
+type SmartkeyDefinitionStorage = {
+  keyDef: SmartkeyDefinition
+  cb: Callback
+}
+
+type SmartkeysContext = {
+  register: (keyDef: SmartkeyDefinition, cb: Callback) => void
+  unregister: (keyDef: SmartkeyDefinition) => void
+  storage: Record<string, SmartkeyDefinitionStorage>
+}
+
+const Smartkeys = createContext<SmartkeysContext>({
+  register: () => {},
+  unregister: () => {},
+  storage: {}
+})
+
+type SmartkeysProviderProps = PropsWithChildren<{
+  keyChainingTime?: number
+}>
+
+function Provider({ children, keyChainingTime = 200 }: SmartkeysProviderProps) {
+  const keyChainingTimeRef = useRef(keyChainingTime)
+
+  const [storage, store] = useState<SmartkeysContext['storage']>({})
+  const storageRef = useRef(storage)
+  storageRef.current = storage
+
+  const [lastKey, storeLastKey] = useState<string[]>([])
+  const lastKeyRef = useRef(lastKey)
+  lastKeyRef.current = lastKey
+
+  const register = useCallback((keyDef: SmartkeyDefinition, cb: Callback) => {
+    store((storage) => ({
+      ...storage,
+      [keyDef.key]: {
+        keyDef,
+        cb
+      }
+    }))
+  }, [])
+
+  const unregister = useCallback((keyDef: SmartkeyDefinition) => {
+    store((storage) => {
+      const ns = { ...storage }
+
+      delete ns[keyDef.key]
+
+      return ns
+    })
+  }, [])
+
+  useEffect(() => {
+    const toHotkeyName = (hotkey: HotKey) => {
+      const name = []
+
+      if (hotkey.ctrlKey || hotkey.metaKey) name.push('mod')
+      if (hotkey.shiftKey) name.push('shift')
+      if (hotkey.altKey) name.push('alt')
+
+      name.push(hotkey.key?.toLocaleLowerCase())
+
+      return name.join('+')
+    }
+
+    const getHotkeyName = (event: KeyboardEvent) => {
+      const eventHotkey = {
+        altKey: event.altKey,
+        ctrlKey: event.ctrlKey,
+        metaKey: event.metaKey,
+        shiftKey: event.shiftKey,
+        key: event.key
+      }
+
+      return toHotkeyName(eventHotkey)
+    }
+
+    const cb = (event: KeyboardEvent) => {
+      const keys = [...lastKeyRef.current, getHotkeyName(event)].filter(
+        (key) => !!key
+      )
+
+      const stored = storageRef.current[keys.join(' ')]
+
+      if (stored) {
+        stored.cb()
+      }
+
+      storeLastKey(keys)
+    }
+
+    document.body.addEventListener('keyup', cb)
+
+    return () => {
+      document.body.removeEventListener('keyup', cb)
+    }
+  }, [])
+
+  useEffect(() => {
+    const timeout = setTimeout(
+      () => storeLastKey([]),
+      keyChainingTimeRef.current
+    )
+
+    return () => clearTimeout(timeout)
+  }, [lastKey])
+
+  return (
+    <Smartkeys.Provider value={{ register, unregister, storage }}>
+      {children}
+    </Smartkeys.Provider>
+  )
+}
+
+type UseSmartkeysProp = SmartkeyDefinition | SmartkeyDefinition[]
+
+export function useSmartkeys(props: UseSmartkeysProp) {
+  const { register, unregister } = useContext(Smartkeys)
+  const [called, call] = useState<string>()
+
+  useEffect(() => {
+    const newSmartKeys = props instanceof Array ? props : [props]
+
+    const cbs = newSmartKeys.map((nsk) => {
+      const cb: Callback = () => {
+        call(nsk.key)
+      }
+
+      register(nsk, cb)
+
+      return nsk
+    })
+
+    return () => {
+      cbs.forEach((cb) => unregister(cb))
+    }
+  }, [props])
+
+  return called
+}
+
+export function useGroupedSmartkeys(): Record<
+  Groups,
+  Pick<
+    SmartkeyDefinition,
+    'key' | ('passthrough' & { hotkey: HotKey | HotKey[] })
+  >[]
+> {
+  const { storage } = useContext(Smartkeys)
+
+  return useMemo(
+    () =>
+      Object.values(storage).reduce(
+        (acc, { keyDef: { group, key, passthrough } }) => ({
+          ...acc,
+          [group]: [
+            ...(acc[group] ?? []),
+            {
+              key,
+              ...(passthrough ? { passthrough } : {}),
+              hotkey:
+                key.indexOf(' ') > 0
+                  ? key.split(' ').map((k) => parseHotkey(k))
+                  : parseHotkey(key)
+            }
+          ]
+        }),
+        {}
+      ),
+    [storage]
+  )
+}
+
+export default {
+  Provider
+}
