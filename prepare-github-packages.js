eb16ce08029077bf2f710c6b3c7c637cedc96826
Alexandru Ifrim <alexandru.ifrim@gmail.com>
2022-11-20 20:25:37 +0200

    feat: release
    closes #8

---
+++ prepare-github-packages.js
@@ -0,0 +1,28 @@
+const { writeFileSync, readFileSync } = require('fs')
+const prettier = require('prettier')
+
+const name = process.argv[2]
+
+if (!name) {
+  console.error('Please provide the new package name')
+  console.error('E.g.: node prepare.js react-smartkeys')
+} else {
+  const file = './package/package.json'
+
+  const data = readFileSync(file, {
+    encoding: 'utf-8'
+  })
+
+  const pkg = JSON.parse(data)
+
+  pkg.name = name
+
+  prettier.resolveConfig(file).then((options) => {
+    const formatted = prettier.format(JSON.stringify(pkg), {
+      ...options,
+      parser: 'json-stringify'
+    })
+
+    writeFileSync(file, formatted)
+  })
+}
